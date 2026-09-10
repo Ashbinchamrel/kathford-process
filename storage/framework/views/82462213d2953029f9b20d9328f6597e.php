185e2497c62f2e98ab17a8e3f4ec9001400ebf7d
Ashbin Kumar Chamrel <ashbinkenet@gmail.com>
2026-09-10 16:04:50 +0545

    Ignore Laravel generated storage files

--- storage/framework/views/82462213d2953029f9b20d9328f6597e.php
+++
@@ -1,4 +0,0 @@
-<?php if(isset($workingFiscalYear)): ?>
-<span class="rounded-lg border border-gray-200 bg-gray-50 px-3 py-1.5 text-xs font-semibold text-gray-600">Fiscal year <?php echo e($workingFiscalYear->name); ?><?php echo e($fiscalYearWritable ? ' · Active' : ' · Read-only'); ?></span>
-<?php endif; ?>
-<?php /**PATH /Users/ashbinkumarchamrel/Downloads/kathford-process/resources/views/partials/fiscal-year-selector.blade.php ENDPATH**/ ?>
